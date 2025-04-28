262cd10777ea48040701c78c0d3b19aeece08d78
Truong Nguyen Van <truong.nguyenvan@orientsoftware.com>
2025-04-28 17:58:58 +0700

    fix: basename

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/react-cicd">
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="home" element={<div>Home Page</div>} />
-        <Route path="about" element={<div>About Page</div>} />
+        <Route path="/home" element={<div>Home Page</div>} />
+        <Route path="/about" element={<div>About Page</div>} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
