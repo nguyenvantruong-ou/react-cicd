b0e39311e1dac389cc12e658ad290afeafb277a6
Truong Nguyen Van <truong.nguyenvan@orientsoftware.com>
2025-04-28 18:44:38 +0700

    fix: homepage

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-cicd">
+    <HashRouter basename="/react-cicd">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/home" element={<div>Home Page</div>} />
         <Route path="/about" element={<div>About Page</div>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
